2d29350ea62da6fdb43b59cfcfdf79eef1e2a486
Pallavi Gole <pallavigole2001@gmai.com>
2026-02-20 12:59:02 +0530

    Fix cookie settings for loginUser and adminLogin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,8 @@ const generateToken = (res, payload) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
   });
   return token;
